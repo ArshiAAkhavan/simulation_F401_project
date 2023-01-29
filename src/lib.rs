440ca67532905128685b5c93e6e1411699e40605
ArshiAAkhavan <letmemakenewone@gmail.com>
2023-01-29 19:53:32 +0330

    export all queues

--- src/lib.rs
+++ src/lib.rs
@@ -174,9 +174,15 @@ impl<Dispatcher> Scheduler<Dispatcher> {
 
     pub fn export(&self, path: &Path) {
         let mut wtr = Writer::from_path(path).unwrap();
-        for t in &self.done {
+        for t in self
+            .done
+            .iter()
+            .chain(self.q1.tasks.iter())
+            .chain(self.q2.tasks.iter())
+            .chain(self.q3.tasks.iter())
+            .chain(self.priority_queue.iter())
+        {
             let _ = wtr.serialize(t.export());
         }
-        //TODO export other queues
     }
 }
